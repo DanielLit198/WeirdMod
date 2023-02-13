82aff940fea79c36065c882432706d0f0e2bbc75
test <705645504@qq.com>
2023-02-13 13:59:31 +0800

    upload

---
+++ src/main/java/com/daniel/weirdmod/weirdmod/Weirdmod.java
@@ -0,0 +1,10 @@
+package com.daniel.weirdmod.weirdmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Weirdmod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/daniel/weirdmod/weirdmod/client/WeirdmodClient.java
@@ -0,0 +1,13 @@
+package com.daniel.weirdmod.weirdmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class WeirdmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
